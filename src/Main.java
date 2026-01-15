f224e0c8da7cb63c4ebadbfaf1a73bc36772dd23
Anthony <anthonydacsilva10@gmail.com>
2026-01-15 18:29:17 -0300

    feat: implement arrow-based PIN decoder challenge

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import desafios.KataDecArrowPinCode;
+
+public class Main {
+    public static void main(String[] args) {
+        KataDecArrowPinCode kataDecArrowPinCode = new KataDecArrowPinCode();
+    }
+}
