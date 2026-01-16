fb5b96e30e39b94691cba44c792bf04bee1cb702
Anthony <anthonydacsilva10@gmail.com>
2026-01-16 15:58:22 -0300

    feat: implement "Playing With Digits" Kata from Codewars

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import desafios.KataDecArrowPinCode;
+import desafios.KataPlayingWithDigits;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(KataDecArrowPinCode.decArrowPinCode("1→↑→")));
+        System.out.println(KataPlayingWithDigits.digPow(46288, 3));
     }
 
 }
